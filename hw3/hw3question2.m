3eff3d3b6fedb70c20dac2260dbbac7215efdec9
hornbd96 <hornbd96@gmail.com>
2018-03-05 00:40:04 -0500

    dead

--- hw3/hw3question2.m
+++ hw3/hw3question2.m
@@ -10,4 +10,30 @@ testClassTable = table2cell(bcpTable(133:198, 3));
 % classTable = table2cell(classTable);
 % dataTable = table2array(dataTable);
 
-tree = fitrtree(trainDataTable, trainClassTable);
+% tree = fitrtree(trainDataTable, trainClassTable);
+
+% Linear Regression for a single X, Y
+trainSingleData = table2array(bcpTable(1:132, 4)); % just for column 4
+trainSingleClass = table2array(bcpTable(1:132, 3));
+tempSumDataClass = trainSingleData.*trainSingleClass;
+tempDataSquared = trainSingleData .* trainSingleData;
+tempClassSquared = trainSingleClass .* trainSingleClass;
+newTable = [trainSingleData, trainSingleClass, tempSumDataClass,tempDataSquared, tempClassSquared];
+sumTable = sum(newTable);
+
+n = length(trainSingleClass) % dont hardcode this homie
+
+
+aNumerator = (sumTable(2) * sumTable(4)) - (sumTable(1)*sumTable(3));
+aDenominator = n*sumTable(4)- sumTable(1).^2;
+aValue = aNumerator/aDenominator
+
+bNumerator = n*sumTable(3)-(sumTable(1)*sumTable(2));
+bValue = bNumerator/aDenominator
+
+disp('Y = a + b*X')
+
+
+% confirm
+
+[r,m,b] = regress(trainSingleData,trainSingleClass)
